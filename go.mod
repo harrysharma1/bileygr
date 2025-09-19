dbdc5c690a1b6b9baa51fdc2af4edad8cf5b34f2
Harry Sharma <harrysharma1066@gmail.com>
2025-09-19 14:15:05 +0100

    feat: starting mal manga for it to then be scraped from weeb central

--- go.mod
+++ go.mod
@@ -15,9 +15,18 @@ require (
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/jawher/mow.cli v1.2.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.13.4 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.42.0 // indirect
 	golang.org/x/net v0.44.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/protobuf v1.36.9 // indirect
--- scraper/weebcentralscraper/weebCentralScraper.go
+++ scraper/weebcentralscraper/weebCentralScraper.go
@@ -3,38 +3,14 @@ package weebcentralscraper
 import (
 	"fmt"
 	"log"
-	"strings"
 
 	"github.com/darenliang/jikan-go"
-	"github.com/gocolly/colly"
 )
 
 func MAL() {
-	anime, err := jikan.GetAnimeById(1)
+	manhwa, err := jikan.GetTopManga(jikan.TopMangaTypeManhwa, jikan.TopMangaFilterByPopularity, 1)
 	if err != nil {
 		log.Fatalf("error getting anime, %e", err)
 	}
-
-	fmt.Println(anime)
-}
-
-func WeebCentralScrape() {
-	c := colly.NewCollector()
-
-	c.OnHTML("a[href]", func(h *colly.HTMLElement) {
-		link := h.Attr("href")
-		if strings.Contains(link, "/chapters/") {
-			fmt.Printf("Chapter Link found: %s\n", link)
-		}
-	})
-
-	c.OnError(func(r *colly.Response, err error) {
-		log.Printf("Request URL: %s failed with response: %v\nError: %v", r.Request.URL, r, err)
-	})
-
-	err := c.Visit("https://weebcentral.com/series/01J76XYBD282Y3XKX0KRGD64Q5/full-chapter-list")
-	if err != nil {
-		log.Fatalf("error visiting Lookism: %e", err)
-	}
-
+	fmt.Println(manhwa.Data)
 }
